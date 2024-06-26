b271a2b9b91d739b1fb00a97b0c2495e0ba35764
James Michael Brandewie <ja33364@g64-vdi-rh9-002.llan.ll.mit.edu>
2024-06-26 08:56:55 -0400

    Not sure, most RL stuff backbone wise is implemented

---
+++ myClasses/Packet.py
@@ -0,0 +1,49 @@
+import pdb 
+
+class Packet: 
+    """
+    This class represents a packet.
+
+    Inputs: 
+    routingMetadata: depends on the routing method. has 
+
+    """
+    
+    def __init__(self,
+                 startLocation,
+                 endLocation,
+                 startSat,
+                 endSat, 
+                 currSat, 
+                 packetSendTime,
+                 routingMetadata,
+                 packetArriveTime
+                 ):
+        self.startLocation = startLocation
+        self.endLocation = endLocation
+
+        self.startSat = startSat
+        self.endSat = endSat
+        self.currSat = currSat
+
+        self.packetSendTime = packetSendTime
+        self.routingMetadata = routingMetadata
+        self.packetArriveTime = packetArriveTime
+
+        #this is only used within the "basic" protocol
+        self.routingMetadata["currIndInPath"] = 0
+
+    def reachedEnd(self):
+        return self.endSat == self.currSat
+        
+    def returnNextHopBasic(self): 
+        return self.routingMetadata["path"][self.routingMetadata["currIndInPath"]+1]
+
+    def updateToNextHopBasic(self): 
+        self.routingMetadata["currIndInPath"]+=1 
+        self.currSat = self.routingMetadata["path"][self.routingMetadata["currIndInPath"]]
+
+    def __str__(self):
+        return vars(self)
+
+        
---
+++ myClasses/RoutingRL.py
@@ -0,0 +1,168 @@
+#Hello! This class is for the RL agent. 
+#we are defining everything in terms of our personal satellite :))
+#very high coupling between this RL agent and the satellite, as many variables are basically the same 
+
+#first, get the satellite class 
+import myClasses.Player as Player
+
+#make imports for the gym environment and plots 
+import numpy as np 
+ 
+import matplotlib.pyplot as plt
+import PIL.Image as Image
+import gym
+import random
+
+from gym import Env, spaces
+import time
+import copy
+
+#make imports for processing and models 
+import random
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import pdb 
+
+#transition consists of state, action, next state, reward
+#just a tuple of information 
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+#create replay object 
+class ReplayMemory(object):
+
+    #create basic object, dequer for actual memory 
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    #put a transition/experience on the buffer 
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    #randomly sample from our past memory for an amount = batch size 
+    #sample without replacement, but doesnt actually remove from the buffer/sampling 
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    #get the length of the buffer. not sure if this is calculated every time 
+    def __len__(self):
+        return len(self.memory)
+    
+
+#create DQN network 
+class DQN(nn.Module):
+
+    #create the initial DQN network
+    #just 3 layers, with the input being the observations, and the actions being the output
+    def __init__(self, n_observations, n_actions):
+        #initialize network 
+        super(DQN, self).__init__()
+
+        #create layers 
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+
+    #forward pass through the network, using the basic input 
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+    
+#
+class DQNAgentRouting: 
+    
+    def __init__(self, satellite): 
+
+        #set up hardware:
+        # if GPU is to be used
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        #store the satellite this agent gives routing info to 
+        self.satellite = satellite
+
+        #setup hyperparameters; used for training 
+        self.BATCH_SIZE = 128
+        self.GAMMA = 0.99
+        self.EPS_START = 0.9
+        self.EPS_END = 0.05
+        self.EPS_DECAY = 1000
+        self.TAU = 0.005
+        self.LR = 1e-4
+
+        #from the satellite, get the number of possible actions and the shape of the observation space
+        #get number of actions from connected players
+        n_actions = len(self.satellite.connectedToPlayers)
+        #get size of observation space from the adjMatrix and QLengths (should be equal)
+        #TODO: look at observation compression. Only in nearby vicinity most likely matters 
+        n_observations = np.size(self.satellite.adjMatrix)
+        n_observations+= np.size(self.satellite.QFinishTimes)
+
+        #policy net = network we use to make our decisions. its the one that we use forward passes to interact with the environment
+        #target net = network we use to train upon i.e. the network that generates the target that we use to update the policy net 
+        self.policy_net = DQN(n_observations, n_actions).to(self.device)
+        self.target_net = DQN(n_observations, n_actions).to(self.device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        #create optimizer and buffer 
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
+        self.memory = ReplayMemory(10000)
+
+    #create function for selecting action based on state 
+    def select_action(self, packet):
+        """
+        Function to select action based on policy using an input packet and the current environment state. 
+
+        Inputs:
+        packet: meta data on packet 
+
+        Outputs: 
+        action: what satellite to route the packet towards 
+
+        """
+        #for now, take random action 
+        #can optimize later... ('state', 'action', 'next_state', 'reward'))
+        
+        #select random action 
+        #hehe
+        action = torch.tensor([[self.env.action_space.sample()]], device=self.device, dtype=torch.long) 
+        
+        #create experience from adjMatrix and QLengths
+        adjMatrixState = np.ravel(copy.deepcopy(self.satellite.adjMatrix))
+        queueLengthState = copy.deepcopy(self.satellite.getQLengths())
+        overallState = np.concatenate([adjMatrixState, queueLengthState])
+
+        #create predicted state (simple for now)
+        predictedState = overallState
+
+        #create experience and push it 
+        self.memory.push(overallState, action, predictedState, None)
+
+        #then, get corresponding action from network and return it 
+        #use GPU if we can
+
+        #first get action from output
+        actionOutput = torch.tensor([[self.policy_net.forward(overallState)]], device=self.device, dtype=torch.long)
+
+        #then, convert it to viable satellite ind 
+        satIndToForwardTo = list(self.satellite.connectedToPlayers)[actionOutput].adjMatPersonalIndex
+
+        pdb.set_trace() 
+
+        #return the viable satellite index now :) 
+        return satIndToForwardTo 
+        
+        #torch.tensor([[self.policy_net.forward(overallState)]], device=self.device, dtype=torch.long)
+    
+    def optimize(self):
+        return 
